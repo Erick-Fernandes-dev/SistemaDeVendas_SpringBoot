91c8698914921a31ad80bcba5e4df05523a3aa8e
ErickFernandes-Algnologia <erick.farias@dcx.ufpb.br>
2021-05-02 15:11:08 -0300

    Update: Spring Boot Project

--- src/main/java/io/github/erickFernandesfps/VendasAplication.java
+++ src/main/java/io/github/erickFernandesfps/VendasAplication.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasAplication {
-
+    /**
+     *
+     * @param args
+     * fornece uma maneira conveniente de inicializar um aplicativo Spring que é iniciado a partir de
+     * um método main (). Em muitas situações,você pode delegar ao método estático SpringApplication.run
+     * , conforme mostrado no exemplo a seguir
+     */
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
 
@@ -21,7 +27,7 @@ public class VendasAplication {
     private String applicationName;
 
     @GetMapping("/hello")
-    public String helloWorld(){
+    public String helloWorld() {
         return applicationName;
     }
 
---
+++ src/main/java/io/github/erickFernandesfps/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.erickFernandesfps.repository;
+
+import io.github.erickFernandesfps.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        // acessa a base e salvar cliente
+    }
+}
