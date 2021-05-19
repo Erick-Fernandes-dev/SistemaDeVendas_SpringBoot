36b0aa7fc301ad1e824c89498ba273e740ff0555
ErickFernandes-Algnologia <erick.farias@dcx.ufpb.br>
2021-05-19 10:22:33 -0300

    Update: Project Spring Boot initial

---
+++ src/main/java/io/github/erickFernandesfps/Animal.java
@@ -0,0 +1,5 @@
+package io.github.erickFernandesfps;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/io/github/erickFernandesfps/MinhaConfiguration.java
+++ src/main/java/io/github/erickFernandesfps/MinhaConfiguration.java
@@ -1,8 +1,22 @@
 package io.github.erickFernandesfps;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
+    /*
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "System fail";
+    }*/
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
